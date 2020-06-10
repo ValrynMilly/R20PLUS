0acfa82098c9871af29cb1fb1eb63ac28a5dc07f
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-10 10:28:38 +0100

    SignUP And Login Forms
    Both SignUp And Login Forms are now working and there is a secret key  but posting is not working

--- Main/Main.py
+++ Main/Main.py
@@ -3,8 +3,10 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
+import email_validator
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "SomeSecretKey!"
 Bootstrap(app)
 
 class LoginForm(FlaskForm):
@@ -12,18 +14,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template('login.html', form=form)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    form = RegisterForm()
+    return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
