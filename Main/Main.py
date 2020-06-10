57c92a96e71966c18880a9a05362e108f1d0eddb
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-10 10:01:18 +0100

    Bootstrap & CSS implemented
    Login and Signup is in progress but currently the login page does not work because a secret key is required to use CSRF

--- Main/Main.py
+++ Main/Main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
 @app.route('/')
 def index():
-    return "Hi, Welcome to R20Plus!"
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
